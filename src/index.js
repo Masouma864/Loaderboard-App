0d21c02920d38f84644ab77b61ae18a094c14bba
masouma <masouma2020@gmail.com>
2022-10-19 15:26:03 +0430

    use loader api

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
 import './style.css';
+import postData from './modules/post.js';
+import getData from './modules/get.js';
+
+const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/7thejendiskaoidjd87g87/scores';
+
+
+const name = document.querySelector('#name');
+const score = document.querySelector('#score');
+const refresh = document.querySelector('.btn');
+const addBtn = document.querySelector('.btn-click');
+
+addBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const leaderObj = {
+    user: name.value,
+    score: score.value,
+  };
+
+  postData(url, leaderObj);
+
+  name.value = '';
+  score.value = '';
+});
+
+refresh.addEventListener('click', (e) => {
+  e.preventDefault();
+  getData(url);
+});
